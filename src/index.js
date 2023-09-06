a3e5625c2e794a21e75dc10288f0e4ec16b6ddb5
Schmelzberry <jelsberry96@gmail.com>
2023-09-06 11:10:56 -0700

    initial commit of all 3 of us working on it
    Co-authored-by: Jon Cheng <joncheng-dev@users.noreply.github.com>
    Co-authored-by: pletourneau <pletourneau@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,3 +2,39 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+// Business Logic
+function getBikes(zip) {
+  let apiCall = new XMLHttpRequest();
+  const urlToSend = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${zip}&distance=10&stolenness=proximity
+  `;
+
+  console.log(urlToSend);
+
+  apiCall.addEventListener("loadend", function() {
+    const serverResponse = JSON.parse(this.responseText);
+    if (this.status === 200) {
+      console.log(serverResponse.bikes);
+      showBikes(serverResponse);
+    }
+  });
+  apiCall.open("GET", urlToSend, true);
+  apiCall.send();
+}
+
+// UI LOGIC //
+
+function showBikes(apiResponse) {
+  document.getElementById("showResults").innerText = `The first bike stolen is ${apiResponse.bikes[0].manufacturer_name} ${apiResponse.bikes[0].frame_model}`;
+}
+//add output above to match miro board
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const userEnteredZip = document.getElementById("zipCode").value;
+  document.getElementById("zipCode").value = null;
+  getBikes(userEnteredZip);
+}
+
+window.addEventListener("load", function () {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
