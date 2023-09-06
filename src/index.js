13270c74f5c0f96f4f1b03c5911a545c764996f0
Schmelzberry <jelsberry96@gmail.com>
2023-09-06 13:39:05 -0700

    have functioncal error handling > > Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com> Co-authored-by: Jonathan Cheng <joncheng.dev@gmail.com>

--- src/index.js
+++ src/index.js
@@ -7,17 +7,12 @@ function getBikes(zip) {
   let apiCall = new XMLHttpRequest();
   const urlToSend = `https://bikeindex.org:443/api/v3/search?page=1&per_page=25&location=${zip}&distance=10&stolenness=proximity
   `;
-
-  console.log(urlToSend);
-
   apiCall.addEventListener("loadend", function() {
     const serverResponse = JSON.parse(this.responseText);
     if (this.status === 200) {
-      console.log(serverResponse.bikes);
-      showBikes(serverResponse);
-     
+      showBikes(serverResponse);  
     } else {
-      showError(apiCall, serverResponse);
+      showError(this, serverResponse);
     }
   });
   apiCall.open("GET", urlToSend, true);
@@ -32,13 +27,10 @@ function showBikes(apiResponse) {
     divToUpdate += `Bike stolen are: ${apiResponse.bikes[i].manufacturer_name} ${apiResponse.bikes[i].frame_model} `;
   }
   document.getElementById("showResults").innerText = divToUpdate;
-  console.log(apiResponse.status);
-  console.log(apiResponse.statusText);
 }
 
 function showError(request, apiResponse) {
-  document.getElementById("showResults").innerText = "There's an error.";
-  // document.querySelector("p#showResults").innerText = `There is an error accessing the bike located ${request.status} ${request.statusText}: ${apiResponse.message}`;
+  document.getElementById("showResults").innerText = `There is an error accessing the bike located ${request.status}: ${apiResponse.message}`;
 
 }
 //add output above to match miro board
